43ae38c4e222cd1593da9304b50158e01c97c89f
minchoking <llsky2ll@hanmail.net>
2020-05-13 15:52:03 +0900

    #11 펀딩 상단 참여금액 많은 순으로 정렬 -sh

--- SAW_GOOD/src/main/java/com/saw/good/funding/model/service/FundingServiceImpl.java
+++ SAW_GOOD/src/main/java/com/saw/good/funding/model/service/FundingServiceImpl.java
@@ -1,12 +1,14 @@
 package com.saw.good.funding.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.saw.good.funding.model.dao.FundingDao;
+import com.saw.good.funding.model.vo.FDMember;
 import com.saw.good.funding.model.vo.Funding;
 
 @Service
@@ -19,6 +21,14 @@ public class FundingServiceImpl implements FundingService{
 	FundingDao dao;
 	
 	
+	
+	@Override
+	public List<Funding> selectHighList(int high) {
+		// TODO Auto-generated method stub
+		return dao.selectHighList(session,high);
+	}
+
+
 	@Override
 	public List<Funding> selectList(int numPerPage) {
 		
@@ -32,5 +42,19 @@ public class FundingServiceImpl implements FundingService{
 		return dao.selectItem(session,fdNo);
 	}
 
+
+	@Override
+	public List<FDMember> selectMemberList(int fdNo) {
+		// TODO Auto-generated method stub
+		return dao.selectMemberList(session,fdNo);
+	}
+
+	@Override
+	public Map<String, Integer> selectPriceCount(int fdNo) {
+		// TODO Auto-generated method stub
+		return dao.selectPriceCount(session,fdNo);
+	}
+	
+	
 	
 }
