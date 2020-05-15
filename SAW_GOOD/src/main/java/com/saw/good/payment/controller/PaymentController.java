aae3e334b39de46182b4eb4d38bf18e47aaa7614
승원 <@seungwonlee>
2020-05-15 14:56:35 +0900

    #11 결제페이지 95% 완료 - 승원

--- SAW_GOOD/src/main/java/com/saw/good/payment/controller/PaymentController.java
+++ SAW_GOOD/src/main/java/com/saw/good/payment/controller/PaymentController.java
@@ -1,18 +1,33 @@
 package com.saw.good.payment.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.saw.good.member.model.vo.Member;
+import com.saw.good.payment.model.service.PaymentService;
 @Controller
 public class PaymentController {
+	
+	@Autowired
+	private PaymentService service;
+	
 	@Autowired
 	Logger logger;
 	
 	@RequestMapping("/payment/paymentView")
-	public ModelAndView paymentView(ModelAndView mv) {
-		System.out.println("들어오냐");
+	public ModelAndView paymentView(ModelAndView mv,@SessionAttribute("loginMember") Member m){
+		
+		String userId = m.getUserId();
+		List<Map<String,String>> list = service.selectPayment(userId);
+		System.out.println(list);
+		mv.addObject("list",list);
 		mv.setViewName("payment/payment");
 		return mv;
 	}
