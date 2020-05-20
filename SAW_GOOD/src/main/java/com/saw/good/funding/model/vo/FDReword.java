f3d1f8054f005f145738d4980de18351e96fd2f6
승원 <@seungwonlee>
2020-05-20 14:53:10 +0900

    #19 결제완료 및 vo 작업 완료 - 승원

--- SAW_GOOD/src/main/java/com/saw/good/funding/model/vo/FDReword.java
+++ SAW_GOOD/src/main/java/com/saw/good/funding/model/vo/FDReword.java
@@ -12,4 +12,38 @@ public class FDReword {
 	int fdNo;
 	String reword;
 	int partPrice;
+	public FDReword() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public FDReword(int fdNo, String reword, int partPrice) {
+		super();
+		this.fdNo = fdNo;
+		this.reword = reword;
+		this.partPrice = partPrice;
+	}
+	public int getFdNo() {
+		return fdNo;
+	}
+	public void setFdNo(int fdNo) {
+		this.fdNo = fdNo;
+	}
+	public String getReword() {
+		return reword;
+	}
+	public void setReword(String reword) {
+		this.reword = reword;
+	}
+	public int getPartPrice() {
+		return partPrice;
+	}
+	public void setPartPrice(int partPrice) {
+		this.partPrice = partPrice;
+	}
+	@Override
+	public String toString() {
+		return "FDReword [fdNo=" + fdNo + ", reword=" + reword + ", partPrice=" + partPrice + "]";
+	}
+	
+	
 }
